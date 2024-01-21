53dea4c98426b7c43910d594745954b7f1603523
Chersilnt Ntervisi <c.ntervisi@outlook.com>
2024-01-21 21:52:37 +0000

    added live time in the header section and the past,present and future styling on the divs

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,43 @@
 $(document).ready(function () {
-// Current day and format
+  // Current day and format
 var currentDay = dayjs().format("dddd D MMMM YYYY");
+var currentHour = dayjs().hour("h:mm:ss");
 
-// Update the #currentDay element with the formatted date
+  // Update the #currentDay element with the formatted date
 $("#currentDay").text(currentDay);
+
+ // Function to update the time
+ function updateTime() {
+    var currentTime = dayjs().format("h:mm:ss A");
+    $("#currentTime").text(currentTime);
+ }
+
+ // Call the updateTime function every second
+ setInterval(updateTime, 1000);
+
+$(".time-block").each(function () {
+var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    if (timeBlockHour < currentHour) {
+    $(this).addClass("past");
+    } else if (timeBlockHour === currentHour) {
+    $(this).addClass("present");
+    } else {
+    $(this).addClass("future");
+    }
+
+// Load saved events from local storage
+    var timeBlockId = $(this).attr("id");
+    var savedEvent = localStorage.getItem(timeBlockId);
+    if (savedEvent !== null) {
+    $(this).find(".description").val(savedEvent);
+    }
+});
+
+// Add event listener to save buttons
+$(".saveBtn").click(function () {
+    var timeBlockId = $(this).parent().attr("id");
+    var textAreaValue = $(this).prev(".description").val();
+    localStorage.setItem(timeBlockId, textAreaValue);
+});
 });
